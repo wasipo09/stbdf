9b2c9f395483839b9ba38f7904b3c93cd21c11d6
Wasin Pongmussaya <pwnlowed@icloud.com>
2023-02-06 02:53:00 +0700

    Hello

--- program/func_bot_agent.py
+++ program/func_bot_agent.py
@@ -109,6 +109,8 @@ class BotAgent:
     print("---")
     print(f"{self.market_1}: Placing first order...")
     print(f"Side: {self.base_side}, Size: {self.base_size}, Price: {self.base_price}")
+    send_message(f"{self.market_1}: Placing first order...")
+    send_message(f"Side: {self.base_side}, Size: {self.base_size}, Price: {self.base_price}")
     print("---")
 
     # Place Base Order
@@ -143,6 +145,8 @@ class BotAgent:
     print("---")
     print(f"{self.market_2}: Placing second order...")
     print(f"Side: {self.quote_side}, Size: {self.quote_size}, Price: {self.quote_price}")
+    send_message(f"{self.market_2}: Placing second order...")
+    send_message(f"Side: {self.quote_side}, Size: {self.quote_size}, Price: {self.quote_price}")
     print("---")
 
     # Place Quote Order
